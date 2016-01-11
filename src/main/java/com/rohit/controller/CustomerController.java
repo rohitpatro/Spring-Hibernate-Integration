28ed61c50007bce8b151362635d5b0352a0cf2f0
rohit <babun.rohit@gmail.com>
2016-01-11 12:59:14 +0530

    added CustomerService autowiring and homePageRequest() method with home url

--- src/main/java/com/rohit/controller/CustomerController.java
+++ src/main/java/com/rohit/controller/CustomerController.java
@@ -1,14 +1,24 @@
 package com.rohit.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rohit.service.CustomerService;
 
 @Controller
 @RequestMapping("/")
 public class CustomerController {
+	private static Logger logger = Logger.getLogger(CustomerController.class);
 
+	@Autowired
+	CustomerService customerService;
+
+	@RequestMapping("home")
 	public String homePageRequest() {
-		return "index";
+		logger.info("CustomerController :: homePageRequest() method invokede");
+		return "home";
 	}
 
 }
