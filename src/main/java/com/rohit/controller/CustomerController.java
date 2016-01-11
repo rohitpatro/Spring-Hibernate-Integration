014ad7397b3da5b1b7200fca30aac18b4477c857
rohit <babun.rohit@gmail.com>
2016-01-11 15:18:08 +0530

    added getRegisterForm() and register() method for registering the Customer

--- src/main/java/com/rohit/controller/CustomerController.java
+++ src/main/java/com/rohit/controller/CustomerController.java
@@ -3,9 +3,14 @@ package com.rohit.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.rohit.model.Customer;
 import com.rohit.service.CustomerService;
+import com.rohit.util.CustomerConstants;
 
 @Controller
 @RequestMapping("/")
@@ -18,7 +23,26 @@ public class CustomerController {
 	@RequestMapping("home")
 	public String homePageRequest() {
 		logger.info("CustomerController :: homePageRequest() method invokede");
-		return "home";
+		return CustomerConstants.CUSTOMER_HOME;
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.GET)
+	public String getRegisterForm(Customer customer) {
+		logger.info("CustomerController :: getRegisterForm() method invoked");
+		/*
+		 * return new ModelAndView("customer/registerUser", "customer", new
+		 * Customer()); and remove the Customer customer from the argument of
+		 * the method to work as same
+		 */
+		return CustomerConstants.CUSTOMER_REGISTER;
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.POST)
+	public ModelAndView register(@ModelAttribute("customer") Customer customer) {
+		if (customerService.registerCustomer(customer))
+			return new ModelAndView(CustomerConstants.CUSTOMER_HOME, "message", "Successfully Registered");
+		else
+			return new ModelAndView(CustomerConstants.CUSTOMER_REGISTER, "message", "Something Went Wrong");
 	}
 
 }
