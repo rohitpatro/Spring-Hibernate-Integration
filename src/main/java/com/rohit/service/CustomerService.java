74c053cc3761b77907f5709fe5cb3a385984718c
rohit <babun.rohit@gmail.com>
2016-01-11 15:17:19 +0530

    added registration method for registring new Customer

--- src/main/java/com/rohit/service/CustomerService.java
+++ src/main/java/com/rohit/service/CustomerService.java
@@ -1,5 +1,9 @@
 package com.rohit.service;
 
+import com.rohit.model.Customer;
+
 public interface CustomerService {
+
+	public boolean registerCustomer(Customer customer);
 
 }
--- src/main/java/com/rohit/service/CustomerServiceImpl.java
+++ src/main/java/com/rohit/service/CustomerServiceImpl.java
@@ -1,5 +1,22 @@
 package com.rohit.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rohit.dao.CustomerDAO;
+import com.rohit.model.Customer;
+
+@Service("customerService")
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+
+	@Override
+	public boolean registerCustomer(Customer customer) {
+		return customerDAO.registerCustomer(customer);
+	}
 
 }
