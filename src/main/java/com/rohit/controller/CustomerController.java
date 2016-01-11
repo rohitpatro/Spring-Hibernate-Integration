f4de7fd31dd3df18b1c34ddf032c3fec67765c80
rohit <babun.rohit@gmail.com>
2016-01-11 12:33:54 +0530

    added @Controller & @RequestMapping annotation to CustomerController.java

--- src/main/java/com/rohit/controller/CustomerController.java
+++ src/main/java/com/rohit/controller/CustomerController.java
@@ -1,5 +1,14 @@
 package com.rohit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class CustomerController {
+
+	public String homePageRequest() {
+		return "index";
+	}
 
 }
