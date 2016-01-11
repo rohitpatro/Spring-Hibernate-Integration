c867d6d0be81810999e959d62bba96b9e90907d8
rohit <babun.rohit@gmail.com>
2016-01-11 15:16:31 +0530

    added customer registration method to CustomerDAO and it's Implementation

--- src/main/java/com/rohit/dao/CustomerDAO.java
+++ src/main/java/com/rohit/dao/CustomerDAO.java
@@ -1,5 +1,9 @@
 package com.rohit.dao;
 
+import com.rohit.model.Customer;
+
 public interface CustomerDAO {
+
+	boolean registerCustomer(Customer customer);
 
 }
--- src/main/java/com/rohit/dao/CustomerDAOImpl.java
+++ src/main/java/com/rohit/dao/CustomerDAOImpl.java
@@ -1,5 +1,15 @@
 package com.rohit.dao;
 
+import org.springframework.stereotype.Repository;
+
+import com.rohit.model.Customer;
+
+@Repository("customerDAO")
 public class CustomerDAOImpl implements CustomerDAO {
+
+	@Override
+	public boolean registerCustomer(Customer customer) {
+		return customer.getCustomerName().equals(customer.getCustomerPassword());
+	}
 
 }
