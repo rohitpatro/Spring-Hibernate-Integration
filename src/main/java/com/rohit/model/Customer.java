17dd9b177bd8204d750865b385540a4323131aea
rohit <babun.rohit@gmail.com>
2016-01-11 12:02:22 +0530

    added customer properties to Customer.java

--- src/main/java/com/rohit/model/Customer.java
+++ src/main/java/com/rohit/model/Customer.java
@@ -2,4 +2,51 @@ package com.rohit.model;
 
 public class Customer {
 
+	private int customerId;
+	private String customerName;
+	private String customerEmail;
+	private String customerPassword;
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", customerEmail="
+				+ customerEmail + ", customerPassword=" + customerPassword + "]";
+	}
+
+	public Customer() {
+		System.out.println("Customer Constructor Invoked");
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerPassword() {
+		return customerPassword;
+	}
+
+	public void setCustomerPassword(String customerPassword) {
+		this.customerPassword = customerPassword;
+	}
+
 }
