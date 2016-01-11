09599e824a76d7a54ed0e2f196cb0daf58f1d609
rohit <babun.rohit@gmail.com>
2016-01-11 15:14:41 +0530

    added CustomerConstants.java for maintaining the view name as constants

---
+++ src/main/java/com/rohit/util/CustomerConstants.java
@@ -0,0 +1,8 @@
+package com.rohit.util;
+
+public class CustomerConstants {
+
+	public static String CUSTOMER_HOME = "customer/home";
+	public static String CUSTOMER_REGISTER = "customer/registerUser";
+
+}
